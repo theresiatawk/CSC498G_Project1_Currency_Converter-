61b27c49e3b5e28fc32119fb6faf2f09520ee96f
Theresia Tawk <theresiatawk@hotmail.com>
2022-04-01 09:47:51 +0300

    Splash Screen Implemented

--- app/src/main/java/com/lau/currencyconverterapp/MainActivity.java
+++ app/src/main/java/com/lau/currencyconverterapp/MainActivity.java
@@ -2,14 +2,36 @@ package com.lau.currencyconverterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Hiding the Action Bar from the layout
         getSupportActionBar().hide();
+
+        // After 2 seconds it will move from this activity to the home activity (Splash Screen)
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
+
     }
 }
