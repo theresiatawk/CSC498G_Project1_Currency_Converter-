b95421a9bd983f265865e9faf8d58bcd35bf8af1
Theresia Tawk <theresiatawk@hotmail.com>
2022-04-03 00:35:32 +0300

    Implementing post api

--- Backend/post.php
+++ Backend/post.php
@@ -1,5 +1,14 @@
 <?php
+include("db_connection.php");
 
+$currency_rate = $_POST["currency_rate"];
+$amount_to_be_converted = $_POST["amount_to_be_converted"];
+$currency = $_POST["currency"];
+
+
+$query = $mysqli->prepare("INSERT INTO conversions(currency_rate, amount_to_be_converted, currency) VALUES (?, ?, ?);");
+$query->bind_param("iis", $currency_rate, $amount_to_be_converted, $semster);
+$query->execute();
 
 $response = [];
 $response["status"] = "Mabrouk!";
