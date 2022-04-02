9b8d63a593738052fd69efca8046f65ebdbfd671
Theresia Tawk <theresiatawk@hotmail.com>
2022-04-03 00:52:00 +0300

    Adding comments

--- Backend/db_connection.php
+++ Backend/db_connection.php
@@ -5,6 +5,7 @@ $db_user = "root";
 $db_pass = null;
 $db_name = "currencyconcerterdb";
 
+// Establishing connection between the database and the apis
 $mysqli = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
 if(mysqli_connect_errno()){
--- Backend/post.php
+++ Backend/post.php
@@ -5,7 +5,7 @@ $currency_rate = $_POST["currency_rate"];
 $amount_to_be_converted = $_POST["amount_to_be_converted"];
 $currency = $_POST["currency"];
 
-
+// Insert variables in the database
 $query = $mysqli->prepare("INSERT INTO conversions(currency_rate, amount_to_be_converted, currency) VALUES (?, ?, ?);");
 $query->bind_param("iis", $currency_rate, $amount_to_be_converted, $semster);
 $query->execute();
@@ -13,7 +13,7 @@ $query->execute();
 $response = [];
 $response["status"] = "Mabrouk!";
 
-$json_respnse = json_encode($response);
-echo $json_respnse;
+$json_response = json_encode($response);
+echo $json_response;
 
 ?> 
