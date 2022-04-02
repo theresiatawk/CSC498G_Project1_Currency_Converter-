3244af8d6170ffe8a36759ea5ae39a0e98cc7767
Theresia Tawk <theresiatawk@hotmail.com>
2022-04-02 16:31:09 +0300

    Start linking android to api

--- app/src/main/java/com/lau/currencyconverterapp/MainActivity.java
+++ app/src/main/java/com/lau/currencyconverterapp/MainActivity.java
@@ -3,8 +3,16 @@ package com.lau.currencyconverterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -12,7 +20,20 @@ import java.util.TimerTask;
 public class MainActivity extends AppCompatActivity {
 
     Timer timer;
+    // This class is implemented to run functions on the background of our applictaion
+    public class DownloadTask extends AsyncTask<String, Void, String> {
 
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection http;
+            
+            return null;
+        }
+        protected void onPostExecute(String s){
+
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
